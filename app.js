a5102c731452a9026ac89667cd78bc4487b171d2
Carlos Botina <carlosalberto.botinacarpio@gmail.com>
2023-02-02 08:43:53 -0500

    Middlewares added
    error & not found middleware

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
+require('express-async-errors')
+
 const express = require('express')
 const { getTest } = require('./controllers/foodItem')
-const app = express()
 const connectDB = require('./db/connect')
+const app = express()
 const FoodItemRouter = require('./routes/foodItem')
+const NotFoundMiddleware = require('./middleware/not-found')
+const ErrorHandlerMiddleware = require('./middleware/error-handler')
+
 
 app.get('/', (req,res)=> res.end('hello world'))
 app.get('/test', getTest)
- 
-// routes
 
+// routes
 app.use('/api/v1/food', FoodItemRouter)
+
+// Middlewares
+app.use(NotFoundMiddleware)
+app.use(ErrorHandlerMiddleware)
 
 const port = process.env.port || 3000
 
